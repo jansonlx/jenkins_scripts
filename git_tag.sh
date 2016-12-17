9647d5c1ca573c95fd1fb1b393ea57cc57b85e57
Janson L <jansonlx@users.noreply.github.com>
2016-12-17 11:03:20 +0800

    git_tag.sh >>> 重構後第一版（更加靈活適應版本庫的不斷添加）

--- git_tag.sh
+++ git_tag.sh
@@ -4,120 +4,157 @@
 # 腳本：Tag Released Version via Jenkins
 # 功能：為上線後的版本添加標籤
 # 作者：
-#      ____ __   __  __ _____ ___  __  __
-#     /_  /  _ \/ / / / ____/ __ \/ / / /
-#      / / /_/ / /|/ /_/_  / / / / /|/ /
-#   __/ / /-/ / / | /___/ / /_/ / / | /
-#  /___/_/ /_/_/|_|/_____/\____/_/|_|/
+#        ____ __   __  __ _____ ___  __  __
+#       /_  /  _ \/ / / / ____/ __ \/ / / /
+#        / / /_/ / /|/ /_/_  / / / / /|/ /
+#     __/ / /-/ / / | /___/ / /_/ / / | /
+#    /___/_/ /_/_/|_|/_____/\____/_/|_|/
 #
-# 日期：15 Aug 2016
-# 版本：2.0
-# 更新日誌:
-#     15 Aug 2016
-#         + 重構：獨立版本庫，不依賴於其他
-#     27 Jun 2016
-#         + 第一版
-# 使用説明：
-#     在 Jenkins 上新增構建項目前的可選菜單。
-#     其中 Choice Parameter 有：
-#         repo_name：選擇需要操作的 Git 版本庫
-#             選項：列出可操作的 Git 版本庫
-#    String Parameter 有：
-#        tag_name：Git 標籤名稱
-#                  建議：v1.2.0 / v2.3.5
-#        tag_comment：〔選填〕標籤説明
-#                     默認值為「Tag released version '${tag_name}'」
+# 日期：16 Dec 2016
+# 版本：v161216
+# 日誌:
+#     16 Dec 2016
+#         + 重構後第一版（更加靈活適應版本庫的不斷添加）
+# 説明：
+#     所有內容直接複製到 Jenkins 項目的「Execute shell」裏，然後進行以下配置：
+#     1. 添加「String Parameter」
+#        Name: tag_name
+#        Description: Git 標籤名稱
+#                     要求類似：v1.2.0 / v2.3.5
+#     2. 添加「String Parameter」
+#        Name: tag_comment
+#        Description: Git 標籤説明
+#                     建議填寫更新的內容，以供後續參考
+#     3. 添加「Boolean Parameter」（按實際項目，有多少個版本庫就添加多少項）
+#        Name: repo01
+#        Description: google/fonts
+#     4. 修改下文腳本中「repos」參數（和 3 添加的版本庫一一對應）
 #
 ###############################################################################
 
 
-# Git 版本庫地址（不包括版本庫名稱部分）
-git_host="https://user@github.com/user/"
-
-if [[ -z ${repo_name} ]]; then
-    echo -e "\n***** [ERROR] 請選擇需要操作的 Git 版本庫「repo_name」 *****\n"
-    exit 1
-fi
-
+# Jenkins 項目上的「String Parameter」
 if [[ -z ${tag_name} ]]; then
-    echo -e "\n***** [ERROR] 請填寫標籤名稱「tag_name」 *****\n"
-    exit 1
-fi
-
-valid_tag_name=`echo "${tag_name}" | egrep '^v[0-9]+\.[0-9]+\.[0-9]+$' || echo "false"`
-if [[ ${valid_tag_name} == "false" ]]; then
-    echo -e "\n***** [ERROR] 標籤名稱「tag_name」要求格式類似：「v12.34.56」 *****\n"
+    echo -e "\n[Error] >>> 請填寫 Git 標籤名稱「tag_name」\n"
     exit 1
 fi
 
 if [[ -z ${tag_comment} ]]; then
-    # 標籤默認説明
-    tag_comment="Tag released version '${tag_name}'"
+    echo -e "\n[Error] >>> 請填寫 Git 標籤説明「tag_comment」\n"
+    exit 1
+fi
+
+# Git 版本庫地址（不包括版本庫名稱部分）
+git_host="https://user@github.com"
+
+# 關聯數組屬於 Bash 4.0 以上版本的新特性
+# 通過「echo $BASH_VERSION」查看 Bash 版本
+# 關聯數組需要先聲明再使用
+declare -A repos 2>/dev/null || \
+    { echo -e "\n[Error] >>> 此處需要使用關聯數組，請升級 Bash 到 4.0 以上版本\n"; exit 1; }
+# 存放所有的 Git 版本庫名稱（需要包括所屬組織或用户名）
+# 其中「repoxx」同時為 Jenkins 的「Boolean Parameter」，需要一一對應
+repos=( \
+    [repo01]="google/fonts" \
+    [repo02]="macvim-dev/macvim" \
+    )
+
+repos_to_use=()
+# ${!repos[*]} 為 repos 的所有索引
+for key in ${!repos[*]}
+do
+    if [[ ${!key} == "true" ]]; then
+        # 把所有在 Jenkins 上構建項目時選中的版本庫都記錄到「repos_to_use」數組中
+        repos_to_use=(${repos_to_use[*]} ${repos[${key}]})
+    fi
+done
+if [[ -z ${repos_to_use[*]} ]]; then
+    echo -e "\n[Error] >>> 請選擇需要添加標籤的版本庫\n"
+    exit 1
+fi
+
+valid_tag_name=$(echo "${tag_name}" | egrep '^v[0-9]+\.[0-9]+\.[0-9]+$' || echo "false")
+if [[ ${valid_tag_name} == "false" ]]; then
+    echo -e "\n[Error] >>> 標籤名稱「tag_name」要求格式類似：「v12.34.56」\n"
+    exit 1
 fi
 
 
-# 函數：版本庫準備
-# 參數：$1 - git_repo_name - 版本庫名稱
+# 函數：版本庫準備（初始化工作區）
+# 調用：repo_get_ready "參數1"
+#     參數1：版本庫名稱（需要包括所屬組織或用户名）
+# 例子：repo_get_ready "google/fonts"
 repo_get_ready()
 {
-    git_repo_name=$1
-    git_repo="${git_host}/${git_repo_name}.git"
-    echo -e "\n***** 準備「${git_repo_name}」版本庫 *****"
-    cd ${git_repo_name}/ 2>/dev/null || { mkdir ${git_repo_name}/; cd ${git_repo_name}; }
+    local repo_name=$1
+    # 版本庫完整地址
+    local git_repo="${git_host}/${repo_name}.git"
+    # WORKSPACE 是 Jenkins 的系統變量，指向工作目錄
+    # 如同時用到了「Git Merge」那個 Jenkins 項目，
+    # 可以考慮使用同個工作目錄以避免重複 clone 了那麼多個版本庫
+    # 具體做法是用對應的工作目錄絕對路徑代替「WORKSPACE」
+    cd "${WORKSPACE}/${repo_name}" 2>/dev/null || \
+        { mkdir -p ${WORKSPACE}/${repo_name}/; cd "${WORKSPACE}/${repo_name}"; }
     if [[ -d ".git" ]]; then
-        # 切換其他分支之前必需還原對當前工作區的所有更改
-        echo -e "***** 還原對當前工作區的所有更改 *****"
-        # 獲取當前分支名稱
-        current_branch=`git branch | sed -n "s/^\*[ ]*\([^ ]\{1,\}\)$/\1/p"`
-        if [[ -z ${current_branch} ]]; then
-            git reset --hard HEAD
-        else
-            git reset --hard origin/${current_branch}
-        fi
-        echo -e "***** 還原成功 *****\n"
+        # 重置到初始狀態以便於直接切換到其他分支
+        echo -e "\n[Info] >>> 重置當前工作區到初始狀態"
+        git reset --hard HEAD
     else
-        echo -e "***** 首次使用「${git_repo_name}」，克隆版本庫到本地 *****"
-        git clone ${git_repo} ./ || { echo -e "***** [ERROR] 克隆失敗 *****\n"; exit 1; }
-        echo -e "***** 克隆成功 *****\n"
+        echo -e "\n[Info] >>> 首次使用「${repo_name}」，克隆版本庫到本地"
+        git clone ${git_repo} ./ || \
+            { echo -e "\n[Error] >>> 克隆失敗\n"; exit 1; }
     fi
 }
 
 
-# 函數：添加及推送標籤
-# 參數：$1 - tag_name - 標籤名稱
-#       $2 - tag_comment - 標籤説明
+# 函數：添加標籤
+# 調用：git_tag "參數1" "參數2"
+#     參數1：標籤名稱
+#     參數2：標籤説明
+# 例子：git_tag "v1.0.0" "第一個穩定版本上線"
 git_tag()
 {
     if [[ $# -eq 2 ]]; then
-        tag_name=$1
-        tag_comment=$2
+        local tag_name=$1
+        local tag_comment=$2
     else
-        echo "***** [ERROR] 「git_tag」函數的參數個數有誤 *****"
+        echo -e "\n[Error] >>> 「git_tag」函數的參數個數有誤\n"
         exit 1
     fi
-    echo -e "\n***** 將在「master」分支的最新提交上打標籤 *****"
-    git fetch origin
+
+    echo -e "\n[Info] >>> 將在「master」分支的最新提交上添加標籤"
+
+    git fetch -p origin
+
     # 確認標籤是否已存在
-    valid_tag_name=`git tag -l "v*" | egrep "${tag_name}" || echo "true"`
+    local valid_tag_name=`git tag -l "v*" | egrep "${tag_name}" || echo "true"`
     if [[ ${valid_tag_name} != "true" ]]; then
-        echo -e "\n***** [ERROR] 標籤名稱「${tag_name}」已存在 *****\n"
+        echo -e "\n[Error] >>> 標籤名稱「${tag_name}」已存在\n"
         exit 1
     fi
+
     git checkout master
     git pull origin master
-    git tag -a ${tag_name} -m "${tag_comment}" || { echo -e "***** [ERROR] 無法添加標籤「${tag_name}」 *****\n"; exit 1; }
-    git push origin ${tag_name} || { echo -e "***** [ERROR] 無法推送標籤「${tag_name}」 *****\n"; exit 1; }
-    echo "***** 标签添加成功 *****"
+    git tag -a ${tag_name} -m "${tag_comment}" || \
+        { echo -e "\n[Error] >>> 無法添加標籤「${tag_name}」\n"; exit 1; }
+    git push origin ${tag_name} || \
+        { echo -e "\n[Error] >>> 無法推送標籤「${tag_name}」\n"; exit 1; }
+    echo -e " 标签添加成功 *****"
+    echo -e "\n[Info] >>> 標籤「${tag_name}」添加成功\n\n"
 }
 
 
 
-if [[ ${repo_name} == "所有" || ${repo_name} == "first-repo-name" ]]; then
-    repo_get_ready "first-repo-name"
+# 用於計算版本庫個數
+count=0
+for repo_name in ${repos_to_use[*]}
+do
+    let count+=1
+    # ${#repos_to_use[*]} 獲取數組元素個數
+    echo -e "\n[Info] >>> [${count}/${#repos_to_use[*]}] 開始更新版本庫「${repo_name}」"
+    # 函數：版本庫準備（初始化工作區）
+    repo_get_ready ${repo_name}
+    # 函數：添加標籤
     git_tag "${tag_name}" "${tag_comment}"
-fi
-if [[ ${repo_name} == "所有" || ${repo_name} == "second-repo-name" ]]; then
-    repo_get_ready "second-repo-name"
-    git_tag "${tag_name}" "${tag_comment}"
-fi
+done
 
